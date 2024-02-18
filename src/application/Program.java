85b6c758958e420686f50888ffd8c3d9684fff46
Paulo Brusch <contato.paulobrusch@gmail.com>
2024-02-18 00:24:42 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
 import org.w3c.dom.ls.LSOutput;
 
 import java.sql.SQLOutput;
+import java.util.Date;
 
 public class Program {
 
@@ -11,6 +13,9 @@ public class Program {
 
         Department department = new Department(3, "Books");
         System.out.println(department);
+
+        Seller seller = new Seller(21, "Bob","bob@gmail.com", new Date(),3000.0, department);
+        System.out.println(seller);
     }
 
 }
