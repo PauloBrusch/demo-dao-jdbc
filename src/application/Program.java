1adb08c355d51f7af9366d8dc5a48be26c019999
Paulo Brusch <contato.paulobrusch@gmail.com>
2024-02-18 00:43:46 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 import org.w3c.dom.ls.LSOutput;
@@ -16,6 +18,8 @@ public class Program {
 
         Seller seller = new Seller(21, "Bob","bob@gmail.com", new Date(),3000.0, department);
         System.out.println(seller);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
     }
 
 }
